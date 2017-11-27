a8fc732208105efc566e0dad104785f3830cea5d
jam1garner <jam1.mcleod@hotmail.com>
2017-11-27 11:26:36 -0500

    Add install from file or web via command line

--- RivalsModdingTool/Program.cs
+++ RivalsModdingTool/Program.cs
@@ -12,11 +12,13 @@ namespace RivalsModdingTool
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            var mw = new MainWindow();
+            mw.toInstall = args;
+            Application.Run(mw);
         }
     }
 }
